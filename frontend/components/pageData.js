f5e12656014703b2fe20254d403f1ac05ff1b05a
Hardik884 <hardiklohan7@gmail.com>
2025-08-05 23:34:26 +0530

    Added a simple navbar functionality

---
+++ frontend/components/pageData.js
@@ -0,0 +1,24 @@
+
+
+export const pageData = [
+    {
+        name:"Home",
+        path:"/home"
+    },
+    {
+        name:"CreateBlog",
+        path:"/createblog"
+    },
+    {
+        name:"Profile",
+        path:"/profile"
+    },
+    {
+        name:"About Us",
+        path:"/about"
+    },
+    {
+        name:"Contact",
+        path:"/contact"
+    },
+]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,22 @@ import { Home } from "../pages/Home";
 import { Landing } from "../pages/Landing";
 import { Profile } from "../pages/Profile";
 import { ReadBlog } from "../pages/ReadBlog";
+import {Navbar} from "../components/Navbar";
+import { Layout } from '../components/Layout';
+
 function App(){
   return (
     < Router>
       <Routes>
         <Route path="/" element={<Landing/>}/>
+        <Route element={<Layout/>}>
         <Route path="/home" element={<Home/>}/>
         <Route path="/createblog" element={<CreateBlog/>}/>
-        <Route path="/readBlog" element={<ReadBlog/>}/>
+        <Route path="/readBlog/:id " element={<ReadBlog/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        </Route>
       </Routes>
     </Router>
   )
