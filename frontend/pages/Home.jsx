b7df878e99b051aa63d5f774ae492e710883a4ba
Hardik884 <hardiklohan7@gmail.com>
2025-08-07 00:31:08 +0530

    Added functionality to the Home Page

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,9 +1,27 @@
-
-
+import {getPosts} from "../api"
+import {useState,useEffect} from "react"
 export function Home(){
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        async function loadAllPosts(){
+            const data = await getPosts()
+            setPosts(data)
+        }
+        loadAllPosts()
+    },[])
     return (
         <>
-            Home Page
+            {posts.map((post) =>{
+                return (
+                    <>
+                    <h1>{post.title}</h1>
+                    <h2>{post.description}</h2>
+                    <p>{post.dateCreated}</p>
+                    </>
+                )
+            })}
         </>
     )
 }
