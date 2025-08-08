73415193a4543143feb9e6587fe5660224644a2b
Hardik884 <hardiklohan7@gmail.com>
2025-08-08 23:57:09 +0530

    Fixed date formatting issues and added a back button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App(){
         <Route element={<Layout/>}>
         <Route path="/home" element={<Home/>}/>
         <Route path="/createblog" element={<CreateBlog/>}/>
-        <Route path="/readBlog/:id " element={<ReadBlog/>}/>
+        <Route path="/readBlog/:id" element={<ReadBlog/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
