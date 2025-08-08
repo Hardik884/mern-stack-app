e1b21c81d1f212947203a0cb8f626e3fcdf729ca
Hardik884 <hardiklohan7@gmail.com>
2025-08-09 00:16:44 +0530

    Gave functionality to the CreateBlog page

--- frontend/pages/CreateBlog.jsx
+++ frontend/pages/CreateBlog.jsx
@@ -1,9 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react"
+import { createPost } from "../src/api"
 
 
 export function CreateBlog(){
+    const [title,setTitle] = useState("")
+    const [description,setDescription] = useState("")
+    const [content,setContent] = useState("")
+    
+    async function handleSubmit(){
+        let submitObject = {
+            title: title,
+            description: description,
+            content: content,
+            author: null,
+            datecreated: new Date()
+        }
+        await createPost(submitObject)
+    }
+
     return (
-        <>
-            CreateBlog Page
-        </>
+        <form onSubmit={handleSubmit}>
+            <label>Blog Post Title: </label>
+            <input onChange ={(e) => setTitle(e.target.value)} maxLength={100} required name="title"/>
+            <label>Blog Description</label>
+            <input onChange ={(e) => setDescription(e.target.value)} maxLength={200} required name="description"/>
+            <label>Blog Content: </label>
+            <textarea onChange ={(e) => setContent(e.target.value)} maxLength={5000} required name="content"/>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
